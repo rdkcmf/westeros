e7ce802944605c2690657abd2128d1bcd02ef849
Jeff Wannamaker <jeff_wannamaker@cable.comcast.com>
2016-03-25 13:55:37 -0400

    Add stubs for westeros-sink for rpi

---
+++ rpi/westeros-sink/westeros-sink-soc.h
@@ -0,0 +1,36 @@
+#ifndef __WESTEROS_SINK_SOC_H__
+#define __WESTEROS_SINK_SOC_H__
+
+#include <stdlib.h>
+
+#include "simplebuffer-client-protocol.h"
+
+#define WESTEROS_SINK_CAPS \
+           "video/x-tbd;"
+
+struct _GstWesterosSinkSoc
+{
+   struct wl_sb *sb;
+   int activeBuffers;
+};
+
+gboolean gst_westeros_sink_soc_init( GstWesterosSink *sink );
+void gst_westeros_sink_soc_term( GstWesterosSink *sink );
+gboolean gst_westeros_sink_soc_null_to_ready( GstWesterosSink *sink, gboolean *passToDefault );
+gboolean gst_westeros_sink_soc_ready_to_paused( GstWesterosSink *sink, gboolean *passToDefault );
+gboolean gst_westeros_sink_soc_paused_to_playing( GstWesterosSink *sink, gboolean *passToDefault );
+gboolean gst_westeros_sink_soc_playing_to_paused( GstWesterosSink *sink, gboolean *passToDefault );
+gboolean gst_westeros_sink_soc_paused_to_ready( GstWesterosSink *sink, gboolean *passToDefault );
+gboolean gst_westeros_sink_soc_ready_to_null( GstWesterosSink *sink, gboolean *passToDefault );
+void gst_westeros_sink_soc_registryHandleGlobal( GstWesterosSink *sink, 
+                                 struct wl_registry *registry, uint32_t id,
+		                           const char *interface, uint32_t version);
+void gst_westeros_sink_soc_registryHandleGlobalRemove(GstWesterosSink *sink,
+                                 struct wl_registry *registry,
+			                        uint32_t name);
+gboolean gst_westeros_sink_soc_accept_caps( GstWesterosSink *sink, GstCaps *caps );
+void gst_westeros_sink_soc_set_startPTS( GstWesterosSink *sink, gint64 pts );
+void gst_westeros_sink_soc_flush( GstWesterosSink *sink );
+gboolean gst_westeros_sink_soc_start_video( GstWesterosSink *sink );
+
+#endif
