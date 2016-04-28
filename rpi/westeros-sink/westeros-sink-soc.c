b6754c9d6fd2bda05c128f1c4952c075ed2511bb
Jeff Wannamaker <jeff_wannamaker@cable.comcast.com>
2016-04-28 13:59:04 -0400

    Support sinks using the render buffer path

--- rpi/westeros-sink/westeros-sink-soc.c
+++ rpi/westeros-sink/westeros-sink-soc.c
@@ -155,6 +155,11 @@ void gst_westeros_sink_soc_set_startPTS( GstWesterosSink *sink, gint64 pts )
    //TBD
 }
 
+void gst_westeros_sink_soc_render( GstWesterosSink *sink, GstBuffer *buffer )
+{
+   //TBD
+}
+
 void gst_westeros_sink_soc_flush( GstWesterosSink *sink )
 {
 }
